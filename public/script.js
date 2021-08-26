0ccc390358d2a91f816dac68f82d3ce251d53f1d
Nev <67838965+NevMoney@users.noreply.github.com>
2021-08-25 21:42:27 -0400

    Update script.js
    getting username didn't work

--- public/script.js
+++ public/script.js
@@ -50,9 +50,7 @@ navigator.mediaDevices
     })
     socket.on('createMessage', (message, userId) => {
       console.log('message', message)
-        console.log('userId', userId)
-        let messenger = await getUsername()
-        console.log('messenger', messenger)
+      console.log('userId', userId)
       if (userId === user) {
         console.log('user is userId')
       }
@@ -71,8 +69,8 @@ socket.on('user-disconnected', (userId) => {
 
 // as soon as we connect using myPeer server and get id, run this code:
 myPeer.on('open', (id) => {
-    user = id
-    console.log("user", id)
+  user = id
+  console.log('user', id)
   socket.emit('join-room', ROOM_ID, id)
   console.log('joined room', ROOM_ID, id)
 })
